cd159a6988c3cb3a5cc1bbb06bd59724ed02633d
ChakradharReddy-Putta <puttachakradharreddy.com>
2020-09-14 20:56:42 +0530

    creating user model by using mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,4 +13,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://Chakradhar:Chinnu@143@cluster0.c2li5.mongodb.net/new?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((result) => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
